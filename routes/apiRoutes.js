f41585f54dc210ba49eafc0203a97977cb4f69ee
Charlie <caam_1275@hotmail.com>
2019-06-27 21:41:59 -0500

    Deleted code from example and changed the document title to MusicPal, search matches index handlebars

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,34 +1,12 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
-
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    console.log(req.body);
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-      console.log(dbExample);
-    }).catch(function(err) {
-      if (err) throw err;
-    });
-  });
-
  //Get all bands 
   app.get("/api/bands", function(req, res){
     db.Band.findAll({}).then(function(bandsdb){
       res.json(bandsdb);
     });
   });
-  /*
-  app.get("/api/bands", function (req, res) {
-    
-  });*/
 
   //Create a new band
   app.post("/api/bands",function(req,res){
@@ -48,13 +26,6 @@ module.exports = function(app) {
   app.post("/api/bands",function(req,res){
     db.Band.create(req.body).then(function(bandsdb){
       res.json(bandsdb);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
     });
   });
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -25,16 +25,6 @@ app.get("/search", function(req, res) {
     });
   });
 })
-  
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
 
   app.get("/form", function(req, res){
     res.render("form");
